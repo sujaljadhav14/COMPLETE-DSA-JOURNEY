c77bbdcfa4e1c0992b335a0a66ffe674fb2d2c2e
Sujal Jadhav <166748974+sujaljadhav14@users.noreply.github.com>
2026-07-01 18:38:14 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.96%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,27 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nextgreat_elem = {}
-        stack = []
-        for i in range(len(nums2) - 1, -1, -1):
-            current = nums2[i]
-            ans = 0
-            while stack and stack[-1]<=current:
+        freq = {}
+        stack =[]
+        ans =[]
+        for i in range(len(nums2)-1,-1,-1):
+            curr = nums2[i]
+            while stack and stack[-1]<=curr:
                 stack.pop()
-            if stack==[]:
-                ans = -1
+            if not stack:
+                freq[curr] = -1
             else:
-                ans = stack[-1]
-            nextgreat_elem[current] = ans
-            stack.append(current)
-        answer = []
-        for num in nums1:
-            answer.append(nextgreat_elem[num])
-        return answer
-
-
-
-
+                freq[curr] = stack[-1]
+            stack.append(curr)
             
-        
-
+        for i in nums1:
+                ans.append(freq[i])
+        return ans
         
