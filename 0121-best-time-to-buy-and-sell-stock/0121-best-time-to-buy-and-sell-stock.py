e6f56bf2f94ba8afeef24d354a376f7dfcba37ad
Sujal Jadhav <166748974+sujaljadhav14@users.noreply.github.com>
2026-08-30 13:12:55 +0530

    Time: 39 ms (72.63%), Space: 28.7 MB (20.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,31 +1,47 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ##optimal solution following the [today's price - cheapest so far] price approach
-        cheapest_price = float("inf")
-        profit = 0
-        for i in range(len(prices)):
-            curr_profit = 0
-            if cheapest_price>prices[i]:
-                cheapest_price = prices[i]
-            curr_profit = prices[i]-cheapest_price
-            if(curr_profit>profit):
-                profit=curr_profit
-        return profit
 
-        #brute force appproach 
-        # min_val = float("inf")
-        # max_val = float("-inf")
-        # minindex = 0
-        # for i in range(len(prices)):
-        #     if prices[i]<min_val:
-        #         min_val = prices[i]
-        #         minindex = i
-        # for i in range(minindex,len(prices)):
-        #     if prices[i]>max_val:
-        #         max_val = prices[i]
+        mini = float("inf")
+        max_prof = float("-inf")
+        for i in prices:
+            if i<mini:
+                mini = i
+            max_prof = max(max_prof , i-mini)
+        return max_prof
         
-        # if (max_val=="-inf"):
-        #     return 0
-        # else:
-        #     return max_val-min_val
-         
+    
+    
+    
+    
+    
+    # def test:
+            
+            # """##optimal solution following the [today's price - cheapest so far] price approach
+            # cheapest_price = float("inf")
+            # profit = 0
+            # for i in range(len(prices)):
+            #     curr_profit = 0
+            #     if cheapest_price>prices[i]:
+            #         cheapest_price = prices[i]
+            #     curr_profit = prices[i]-cheapest_price
+            #     if(curr_profit>profit):
+            #         profit=curr_profit
+            # return profit
+
+            # #brute force appproach 
+            # # min_val = float("inf")
+            # # max_val = float("-inf")
+            # # minindex = 0
+            # # for i in range(len(prices)):
+            # #     if prices[i]<min_val:
+            # #         min_val = prices[i]
+            # #         minindex = i
+            # # for i in range(minindex,len(prices)):
+            # #     if prices[i]>max_val:
+            # #         max_val = prices[i]
+            
+            # # if (max_val=="-inf"):
+            # #     return 0
+            # # else:
+            # #     return max_val-min_val
+            # """
